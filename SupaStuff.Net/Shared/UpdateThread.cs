e74e50f3b4cf0fdfeef76c12e12f7f668285e5ba
SupaMaggie70 <[magnus.larsson.mn@gmail.com]>
2022-06-20 17:21:58 -0500

    Added an auto-update timer

---
+++ SupaStuff.Net/Shared/UpdateThread.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace SupaStuff.Net.Shared
+{
+    public class UpdateThread : IDisposable
+    {
+        public static UpdateThread Instance;
+        private bool running = true;
+        public int updateInterval;
+        public Timer timer;
+        public static UpdateThread MakeUpdateThread(int updateInterval)
+        {
+            if (Instance != null)
+            {
+                Instance.Stop();
+            }
+            Instance = new UpdateThread(updateInterval);
+            return Instance;
+        }
+        private UpdateThread(int updateInterval)
+        {
+            Instance = this;
+            this.updateInterval = updateInterval;
+            timer = new Timer(updateInterval);
+            timer.Elapsed += Update;
+            timer.AutoReset = true;
+        }
+        public void Start()
+        {
+            timer.Enabled = true;
+            running = true;
+        }
+        public void Stop()
+        {
+            timer.Enabled = false;
+            running = false;
+        }
+        public void Update(Object src,ElapsedEventArgs args)
+        {
+            Main.Update();
+        }
+        public void Dispose()
+        {
+            Stop();
+            try
+            {
+                timer.Close();
+                timer.Dispose();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
